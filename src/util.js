d0ac5cda28538d66c67d6f2835b2df277b8eb563
autumn crossan <autumncrossan@autumns-MacBook-Pro-2.local>
2018-03-14 14:25:40 -0400

    added test fixtures, implemented History component

--- src/util.js
+++ src/util.js
@@ -5,14 +5,25 @@ type Point = {
   y: number
 }
 
-export const convertSpaces = (element: string): string =>
+const convertSpaces = (element: string): string =>
   element.replace('%20', '_').replace(' ', '_')
 
-export const angleRadians = (p1: Point, p2: Point): number =>
+const angleRadians = (p1: Point, p2: Point): number =>
   Math.atan2(p2.y - p1.y, p2.x - p1.x)
 
-export const radial = (point: Point, radius: number, radians: number): Point => ({ 
+const radial = (point: Point, radius: number, radians: number): Point => ({ 
   x: Math.cos(radians) * radius + point.x, 
   y: Math.sin(radians) * radius + point.y
 })
+
+const last = <T>(arr: Array<T>): T =>
+  arr[arr.length - 1]
+  
+module.exports = {
+  angleRadians,
+  convertSpaces,
+  last,
+  radial
+}
+
 
